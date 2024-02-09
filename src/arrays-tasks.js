79017e5f4a12e70128d377ddc008af31532b4ec2
Probluesea <danil.kriw@gmail.com>
2024-02-09 21:55:21 +0500

    feat: complete createChunks task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -357,8 +357,15 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const chunkIndexes = arr
+    .filter((_, i) => i % chunkSize === 0)
+    .map((elem) => arr.indexOf(elem));
+
+  return chunkIndexes.reduce((acc, index) => {
+    acc.push(arr.slice(index, index + chunkSize));
+    return acc;
+  }, []);
 }
 
 /**
