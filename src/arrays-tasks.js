65a67099f2e15e790f041a358cd550e1e4577719
Probluesea <danil.kriw@gmail.com>
2024-02-09 23:41:43 +0500

    feat: complete findLongestIncreasingSubsequence task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -528,8 +528,19 @@ function findCommonElements(/* arr1, arr2 */) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  let result = 0;
+
+  nums.reduce((acc, elem, i, arr) => {
+    const elemNext = arr[i + 1];
+
+    if (elem < elemNext) return acc + 1;
+    result = Math.max(result, acc);
+
+    return 1;
+  }, 1);
+
+  return result;
 }
 
 /**
