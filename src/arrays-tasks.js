eac8869540adf258c935a1241ee610705aa99a25
Probluesea <danil.kriw@gmail.com>
2024-02-09 23:04:10 +0500

    feat: complete getIdentityMatrix task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -440,8 +440,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (v, i) => {
+    return Array.from({ length: n }, (_, j) => {
+      return i === j ? 1 : 0;
+    });
+  });
 }
 
 /**
